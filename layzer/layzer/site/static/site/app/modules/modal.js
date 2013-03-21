5ce8dd00ad011ab3044fbfb53b5e26ac5d7a2bbe
Stefan van der Haven <stefan@steeffie.net>
2013-03-21 01:17:06 +0100

    broken

--- layzer/layzer/site/static/site/app/modules/modal.js
+++ layzer/layzer/site/static/site/app/modules/modal.js
@@ -2,33 +2,40 @@
     "use strict";
 
     ng.module('modal', []).directive('modal', ['$compile', function ($compile) {
-
+        var modalScopeAttrs = ['close', 'title', 'submit', 'cancel', 'okText', 'cancelText'];
         var header = $compile(
             '<div class="modal-header">' +
-                '<button type="button" class="close" ng-click="{{modalClose}}">&times;</button>' +
-                '<h3>{{modalTitle}}</h3>' +
+                '<button type="button" class="close" ng-click="{{modal.cancel}};{{modal.close}}">&times;</button>' +
+                '<h3>{{modal.title}}</h3>' +
             '</div>'
         );
         var body = '<div class="modal-body">';
+
+        var backdrop = $compile('<div class="modal-backdrop" ng-click="{{modal.cancel}};{{modal.close}}"></div>');
+        var modal = $compile('<div class="modal">');
         var footer = $compile(
             '<div class="modal-footer">' +
-                '<button class="btn btn-large btn-primary" ng-click="{{modalSubmit}};{{modalClose}}">{{modalOkText}}</button>' +
-                '<button class="btn btn-large" ng-click="{{modalCancel}};{{modalClose}}">{{modalCancelText}}</button>' +
+                '<button class="btn btn-large btn-primary" ng-click="{{modal.submit}};{{modal.close}}">{{modal.okText || \'OK\'}}</button>' +
+                '<button class="btn btn-large" ng-click="{{modal.cancel}};{{modal.close}}">{{modal.cancelText || \'Camcel\'}}</button>' +
             '</div>'
         );
         var link = function (scope, element, attrs) {
-            ng.extend(scope, {
-                modalClose: attrs.close || '',
-                modalTitle: attrs.title || '',
-                modalSubmit: attrs.submit || '',
-                modalCancel: attrs.cancel || '',
-                modalOkText: attrs.okText || 'OK',
-                modalCancelText: attrs.cancelText || 'Cancel'
+            var modalModel = {};
+            ng.forEach(modalScopeAttrs, function(value) {
+                modalModel[value] = attrs[value] || '';
             });
-            element.addClass('modal');
+
+            ng.extend(scope, {
+                modal: modalModel
+            });
+            modal(scope, function (modalClone) {
+                element.children().wrap(modalClone);
+                header(scope, function (clone) { modalClone.prepend(clone); });
+                footer(scope, function (clone) { modalClone.append(clone); });
+            });
             element.children().wrap(body);
-            header(scope, function (clone) { element.prepend(clone); });
-            footer(scope, function (clone) { element.append(clone); });
+            element.children().wrap(modal);
+            backdrop(scope, function (clone) { element.prepend(clone); });
         };
         return {
             link: link
