2bfcc1d0f8e3d33c7865c7d4b663ca7e555db57a
Stefan van der Haven <stefan@steeffie.net>
2013-03-22 17:25:24 +0100

    Better (ie. working) modal

--- layzer/layzer/site/static/site/app/modules/modal.js
+++ layzer/layzer/site/static/site/app/modules/modal.js
@@ -1,38 +1,46 @@
 ;(function (ng) {
     "use strict";
 
-    ng.module('modal', []).directive('modalWindow', ['$compile', function ($compile) {
+    ng.module('modal', []).directive('modalWindow', ['$compile', '$timeout', function ($compile, $timeout) {
         var modalScopeAttrs = ['close', 'header', 'submit', 'cancel', 'okText', 'cancelText'];
-        var modal = $compile(
+        var modal = ng.element(
             '<div class="modal-backdrop" ng-click="{{modal.cancel}};{{modal.close}}"></div>' +
-            '<form class="modal" ng-submit="{{modal.close}}">' +
+            '<form class="modal" ng-submit="{{modal.close}}" name="modalForm" novalidate>' +
                 '<div class="modal-header">' +
-                    '<button type="button" class="close" ng-click="{{modal.cancel}};{{modal.close}}"><span class="iconic iconic-x"></span></button>' +
+                    '<button type="button" class="close" ng-click="{{modal.cancel}};{{modal.close}}"><i class="icon-remove"></i></button>' +
                     '<h3>{{modal.header}}</h3>' +
                 '</div>' +
                 '<div class="modal-body">' +
                 '</div>' +
                 '<div class="modal-footer">' +
-                    '<button class="btn btn-large btn-primary" ng-click="{{modal.submit}}"><span class="iconic iconic-check"></span> {{modal.okText || \'OK\'}}</button>' +
-                    '<button class="btn btn-large" ng-click="{{modal.cancel}}"><span class="iconic iconic-x"></span> {{modal.cancelText || \'Cancel\'}}</button>' +
+                    '<button class="btn btn-large btn-primary" ng-click="{{modal.submit}}" ng-disabled="modalForm.$invalid"><i class="icon-ok"></i> {{modal.okText || \'OK\'}}</button>' +
+                    '<button class="btn btn-large" ng-click="{{modal.cancel}}"><i class="icon-remove"></i> {{modal.cancelText || \'Cancel\'}}</button>' +
                 '</div>' +
             '</form>'
         );
 
-        return function (scope, element, attrs) {
-            var modalModel = {};
-            ng.forEach(modalScopeAttrs, function(value) {
-                modalModel[value] = attrs[value] || '';
-            });
-
-            ng.extend(scope, {
-                modal: modalModel
-            });
-            modal(scope, function (modalClone) {
-                var body = modalClone.find('.modal-body');
-                body.append(element.children());
+        return {
+            compile: function (element) {
+                var modalClone = modal.clone(),
+                    children = element.children(),
+                    body = modalClone.find('.modal-body');
+                body.append(children);
                 element.append(modalClone);
-            });
+                return {
+                    pre: function (scope, element, attrs) {
+
+                        var modalModel = {};
+                        ng.forEach(modalScopeAttrs, function(value) {
+                            modalModel[value] = attrs[value] || '';
+                        });
+
+                        ng.extend(scope, {
+                            modal: modalModel
+                        });
+
+                    }
+                }
+            }
         };
     }]);
 }(angular));
